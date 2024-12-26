47cb22957d2589c0c554f31d689426c903ee6211
pgdai19 <pgdai19@gmail.com>
2024-12-26 16:27:22 +0530

    last commit

--- first.py
+++ first.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welocme to kuberenetes"
+    return "I am bored ... tomorrow I will hire jenkins!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
